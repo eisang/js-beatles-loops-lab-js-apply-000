0a92f986749ce291731c45f4dfbea445a3a7e653
Edwin Moses Isang <edmosesisang@gmail.com>
2018-09-06 07:48:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+var music = [];
+for (var i=0; i < musicians.length; i++)
+music.push(musicians[i] + ' plays ' + instruments[i]);
+  return music;
+}
+theBeatlesPlay("John Lennon plays guitar")
+
+
+function johnLennonFacts(facts) {
+  var info = [];
+  var num = 0
+  while (num < fact.length) {}
+info.push(facts[num] + "!!!");
+ num++
+
+return info;
+
+
+
+
+
+
+
+
+  
